83667ceb892e12cfaaa8aee427c326e8c67ef3b6
Kyle <christiankyleortalla@gmail.com>
2024-11-26 15:24:17 +0800

    samplekey

--- admindashboard.php
+++ admindashboard.php
@@ -23,7 +23,7 @@ if (isset($_POST['logout'])) {
     <script src="https://kit.fontawesome.com/4804625ee9.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="stylesheet" href="admindashboard.css">
-    <title>ADMIN - CTU KEY MANAGEMENT SYSTEM samplw</title>
+    <title>ADMIN - CTU KEY MANAGEMENT SYSTEM</title>
 </head> 
 <body>
 
